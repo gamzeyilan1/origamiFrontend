1d4b50c5e2531a2b8d87b1f88fa14185dbef22a8
Gamze Yilan <gamzeyilan1@gmail.com>
2022-03-16 14:44:00 +0300

    Task module base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {Routes, RouterModule} from "@angular/router";
+import {TaskModuleModule} from "./task-module/task-module.module";
+import {TaskComponent} from "./task-module/task/task.component";
 
-import { AppComponent } from './app.component';
+const routes: Routes = [{ path: 'task-module', loadChildren: () => import('./task-module/task-module.module').then(m => m.TaskModuleModule) }];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TaskModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
